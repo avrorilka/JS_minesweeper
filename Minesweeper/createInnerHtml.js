21eb7513c683f51260ab88c7b21dfe7133c9e07c
avrorilka <avrora758@gmail.com>
2020-03-30 22:55:54 +0300

    changed appearance of mines and refined flag's logic

--- Minesweeper/createInnerHtml.js
+++ Minesweeper/createInnerHtml.js
@@ -1,10 +1,11 @@
-let defaultRows = 8;
-let defaultCols = 8;
-let defaultMines = 8;
+let defaultRows = 8, defaultCols = 8, defaultMines = 12;
+let rowsElem, colsElem, minesElem;
+let boardArr = [];
+
 defaultCreator();
-let rowsElem = document.querySelector('#rows');
-let colsElem = document.querySelector('#cols');
-let minesElem = document.querySelector('#mines');
+
+let generateButtonElem = document.querySelector('#submit');
+let generatedBlockElem = document.querySelector('#generatedBlock');
 
 function defaultCreator() {
     document.body.innerHTML = "<h1>Minesweeper</h1>";
@@ -17,7 +18,6 @@ function defaultCreator() {
     drawTable(defaultCols, defaultRows, defaultMines);
     drawOptions();
 }
-
 function drawOptions() {
     let optionDivElem = document.createElement('div');
     optionDivElem.id = 'options';
@@ -30,7 +30,7 @@ function drawOptions() {
     l = document.createElement('input');
     l.setAttribute('type',"number");
     l.id = 'cols';
-    l.setAttribute('value',+8);
+    l.setAttribute('value',+defaultRows);
     l.setAttribute('max',+50);
     l.setAttribute('min',+3);
     optionDivElem.appendChild(l);
@@ -41,7 +41,7 @@ function drawOptions() {
 
     l = document.createElement('input');
     l.setAttribute('type',"number");
-    l.setAttribute('value',+8);
+    l.setAttribute('value',+defaultCols);
     l.id = 'rows';
     l.setAttribute('max',+50);
     l.setAttribute('min',+3);
@@ -54,7 +54,7 @@ function drawOptions() {
     l = document.createElement('input');
     l.setAttribute('type',"number");
     l.id = "mines";
-    l.setAttribute('value',+8);
+    l.setAttribute('value',+defaultMines);
     l.setAttribute('max',+2500);
     l.setAttribute('min',+1);
     optionDivElem.appendChild(l);
@@ -74,9 +74,7 @@ function drawOptions() {
 
 }
 function drawTable(cols, rows, mines) {
-
-    let boardArr = new Array(cols);
-
+    boardArr = new Array(cols);
     let wholeTable = document.createElement('div');
     wholeTable.className = 'table';
     wholeTable.id = 'generatedBlock';
@@ -101,59 +99,59 @@ function drawTable(cols, rows, mines) {
     return wholeTable;
 }
 
-let generateButtonElem = document.querySelector('#submit');
-let generatedBlockElem = document.querySelector('#generatedBlock');
-
 generateButtonElem.addEventListener("click", function () {
-    let rows = +rowsElem.value;
-    let cols = +colsElem.value;
-    let mines = +minesElem.value;
-    if (+(rows * cols) > mines){
-        generatedBlockElem.innerHTML = '';
-        generatedBlockElem.appendChild(drawTable(rows, cols, mines));
-        document.querySelector("#gameInfoBox").innerHTML = "<p class='flagsText'>" + defaultMines + " Flags left!</p>";
+     rowsElem = +(document.querySelector('#rows').value);
+     colsElem = +(document.querySelector('#cols').value);
+     minesElem = +(document.querySelector('#mines').value);
 
+    if (rowsElem * colsElem > minesElem) {
+        generatedBlockElem.innerHTML = '';
+        generatedBlockElem.appendChild(drawTable(+rowsElem, +colsElem, +minesElem));
+
+        document.querySelector("#gameInfoBox").innerHTML = "<p class='flagsText'>" + minesElem + " Flags left!</p>";
     }
     else alert("Error! Wrong number of bombs!")
 });
+
+minesElem = +(document.querySelector('#mines').value);
 generatedBlockElem.addEventListener('contextmenu', function(event) {
     event.preventDefault();
-    let element = event.target;
-    let flagCount = 0;
+    let element = event.target, flagCount = 0;
 
-    if (event.target.tagName === 'DIV') {
+    if (element.tagName === 'DIV' && element.classList.contains("hide")) {
         element.classList.toggle('flag');
         flagCount = +(document.getElementsByClassName("flag").length);
 
-        if (+flagCount === +(minesElem.value)+1) {
+        if (+flagCount === minesElem + 1) {
             alert("No flags left");
+
             element.classList.toggle('flag');
             document.querySelector("#gameInfoBox").innerHTML = "<p class='flagsText'> 0 Flags left!</p>";
         }
         else
-            document.querySelector("#gameInfoBox").innerHTML = "<p class='flagsText'>" + ((+minesElem.value)- (+flagCount)) + " Flags left!</p>";
+            document.querySelector("#gameInfoBox").innerHTML = "<p class='flagsText'>" +(minesElem - flagCount) + " Flags left!</p>";
     }
 });
-generatedBlockElem.addEventListener('click', function showCellInfo(event) {
-    let rows = +rowsElem.value;
-    let cols = +colsElem.value;
-    let mines = +minesElem.value;
 
+generatedBlockElem.addEventListener('click', function showCellInfo(event) {
+    event.preventDefault();
     let element = event.target;
-    if (event.target.tagName === 'DIV') {
+
+    if (element.tagName === 'DIV' && !element.classList.contains("flag")) {
         element.classList.remove("hide");
 
         if (element.dataset.state === "mine") {
             alert("Game Over!");
+
            let minesList = document.getElementsByClassName("mine");
             for (var i = 0; i < minesList.length; i++) {
                 minesList[i].classList.remove("hide");
+
+                if (minesList[i].classList.contains("flag")) {
+                    minesList[i].classList.toggle('plantedFlag');
+                }
             }
         }
-        if (element.dataset.state === "empty") {
-
-        }
-        event.preventDefault();
     }
 
 });
